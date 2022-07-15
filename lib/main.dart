d6904f21bfc8df739177869da474dfb20a4f9f82
Aleksandr Pugachev <aleksandr.pugachev@id.thesoul.io>
2022-07-15 17:06:29 +0300

    remove unused code

--- lib/main.dart
+++ lib/main.dart
@@ -23,83 +23,50 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({Key? key}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final List<WordPair> items = List.empty(growable: true);
-
-  void _incrementCounter() {
-    setState(() {
-      items.setAll(0, List.generate(100, (index) => WordPair.random()));
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('List'),
+        title: const Text('Random nicknames'),
       ),
-      body: ListView.builder(
-        itemCount: items.length,
-        itemBuilder: ((context, index) {
-          if (index.isOdd) {
-            return const Divider(
-              thickness: 1.0,
-            );
-          }
+      body: const Randomazer(),
+    );
+  }
+}
 
-          final item = items[index];
-          return ListTile(
-            title: Text(item.asPascalCase),
+class Randomazer extends StatefulWidget {
+  const Randomazer({Key? key}) : super(key: key);
+
+  @override
+  State<Randomazer> createState() => _RandomazerState();
+}
+
+class _RandomazerState extends State<Randomazer> {
+  final List items = <WordPair>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemBuilder: ((context, index) {
+        if (index.isOdd) {
+          return const Divider(
+            thickness: 1.0,
           );
-        }),
-      ),
-      // Column(\]
-      //   // Column is also a layout widget. It takes a list of children and
-      //   // arranges them vertically. By default, it sizes itself to fit its
-      //   // children horizontally, and tries to be as tall as its parent.
-      //   //
-      //   // Invoke "debug painting" (press "p" in the console, choose the
-      //   // "Toggle Debug Paint" action from the Flutter Inspector in Android
-      //   // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-      //   // to see the wireframe for each widget.
-      //   //
-      //   // Column has various properties to control how it sizes itself and
-      //   // how it positions its children. Here we use mainAxisAlignment to
-      //   // center the children vertically; the main axis here is the vertical
-      //   // axis because Columns are vertical (the cross axis would be
-      //   // horizontal).
-      //   mainAxisAlignment: MainAxisAlignment.center,
-      //   children: <Widget>[
-      //     const Text(
-      //       'You have clicked the button this many times:',
-      //     ),
-      //     Text(
-      //       '$_counter',
-      //       style: Theme.of(context).textTheme.headline4,
-      //     ),
-      //     Text('$_counter', style: Theme.of(context).textTheme.bodyText2)
-      //   ],
-      // ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.apple),
-      ),
+        }
+
+        int wordIndex = index ~/ 2;
+
+        if (wordIndex >= items.length) {
+          items.addAll(generateWordPairs().take(10));
+        }
+
+        return ListTile(
+          title: Text(items[wordIndex].asPascalCase),
+        );
+      }),
     );
   }
 }
