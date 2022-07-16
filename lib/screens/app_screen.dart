ee2dbcf7e8c438b73a655878d94395569fcc3bb8
Aleksandr Pugachev <aleksandr.pugachev@id.thesoul.io>
2022-07-16 13:39:11 +0300

    move to BotoomNavigationBar

--- lib/screens/app_screen.dart
+++ lib/screens/app_screen.dart
@@ -2,7 +2,6 @@ import 'package:flex_color_scheme/flex_color_scheme.dart';
 import 'package:flutter/material.dart';
 import 'package:inspire/screens/home_screen.dart';
 import 'package:inspire/screens/settings.dart';
-import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
 class AppScreen extends StatefulWidget {
   const AppScreen({Key? key}) : super(key: key);
@@ -14,19 +13,15 @@ class AppScreen extends StatefulWidget {
 class _AppScreenState extends State<AppScreen>
     with SingleTickerProviderStateMixin {
   int _currentIndex = 0;
-  late TabController _tabController;
 
-  @override
-  void initState() {
-    super.initState();
+  static const List<Widget> _widgetOptions = <Widget>[
+    Randomazer(),
+    SettingsScreen(),
+  ];
 
-    _tabController = TabController(length: 2, vsync: this, initialIndex: 0);
-    _tabController.addListener(_handleTabSelection);
-  }
-
-  _handleTabSelection() {
+  _handleTabSelection(int index) {
     setState(() {
-      _currentIndex = _tabController.index;
+      _currentIndex = index;
     });
   }
 
@@ -39,71 +34,20 @@ class _AppScreenState extends State<AppScreen>
       darkTheme: FlexThemeData.dark(scheme: FlexScheme.mandyRed),
       themeMode: ThemeMode.system,
       home: Scaffold(
-        bottomNavigationBar: BottomAppBar(
-          elevation: 0,
-          shape: const CircularNotchedRectangle(),
-          child: Container(
-            height: 50,
-            child: TabBar(
-              controller: _tabController,
-              indicator: const BoxDecoration(),
-              tabs: <Widget>[
-                Container(
-                  child: (_currentIndex == 0)
-                      ? Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            const Icon(
-                              MdiIcons.home,
-                            ),
-                            Container(
-                              decoration: const BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(2.5))),
-                              height: 5,
-                              width: 10,
-                            )
-                          ],
-                        )
-                      : const Icon(
-                          MdiIcons.home,
-                        ),
-                ),
-                Container(
-                  child: (_currentIndex == 1)
-                      ? Column(
-                          mainAxisSize: MainAxisSize.min,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            const Icon(
-                              MdiIcons.cog,
-                            ),
-                            Container(
-                              decoration: const BoxDecoration(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(2.5))),
-                              height: 5,
-                              width: 10,
-                            )
-                          ],
-                        )
-                      : const Icon(
-                          MdiIcons.cog,
-                        ),
-                ),
-              ],
-            ),
+          bottomNavigationBar: BottomNavigationBar(
+            elevation: 0,
+            currentIndex: _currentIndex,
+            onTap: _handleTabSelection,
+            items: const <BottomNavigationBarItem>[
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.settings), label: 'Settings'),
+            ],
           ),
-        ),
-        body: TabBarView(
-          controller: _tabController,
-          children: const <Widget>[
-            Randomazer(),
-            SettingsScreen(),
-          ],
-        ),
-      ),
+          body: Center(child: _widgetOptions.elementAt(_currentIndex))),
     );
   }
 }
